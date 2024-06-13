9e9fdffc5531b0e967e24dfdbf5a0ea6d2c47407
Beryl <berylyenos1@gmail.com>
2024-06-13 12:16:23 +0700

    update 13/6/2024

--- Assets/Scripts/elevator_controller.cs
+++ Assets/Scripts/elevator_controller.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class elevator_controller : MonoBehaviour
+public class ElevatorController : MonoBehaviour
 {
     public Collider buttonCollider;
     public Collider playerRightHandCollider;
     public Collider playerLeftHandCollider;
+    public GameObject targetObject; // The object to teleport
+    public Transform teleportDestination; // The destination to teleport to
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,20 @@ public class elevator_controller : MonoBehaviour
         if (buttonCollider.bounds.Intersects(playerRightHandCollider.bounds) || buttonCollider.bounds.Intersects(playerLeftHandCollider.bounds))
         {
             Debug.Log("Button pressed");
+            TeleportObject();
+        }
+    }
+
+    void TeleportObject()
+    {
+        if (targetObject != null && teleportDestination != null)
+        {
+            targetObject.transform.position = teleportDestination.position;
+            Debug.Log("Object teleported to destination");
+        }
+        else
+        {
+            Debug.LogWarning("Target object or teleport destination not set");
         }
     }
 }
