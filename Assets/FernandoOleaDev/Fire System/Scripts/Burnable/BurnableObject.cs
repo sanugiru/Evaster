ea295518e5c68c4d7ba8db2757e4ec406003f825
Shahnaaz Firdaus <72660860+sanugiru@users.noreply.github.com>
2024-06-14 11:18:41 +0700

    Build version 1.0.0

--- Assets/FernandoOleaDev/Fire System/Scripts/Burnable/BurnableObject.cs
+++ Assets/FernandoOleaDev/Fire System/Scripts/Burnable/BurnableObject.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditorInternal;
 #if UNITY_EDITOR
+using UnityEditorInternal;
 using System.IO;
 using UnityEditor;
 #endif
